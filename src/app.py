b1a0f8c5150a23f1421ab32d13bf4b4a8e066d3c
Anubhav Y <contact+code@anubhav.codes>
2020-04-21 09:15:21 +0200

    👁 Fixed some bugs.

--- src/app.py
+++ src/app.py
@@ -16,10 +16,10 @@ HEADERS = {"User-Agent": "github.com/anubhavcodes/socialkarma"}
 
 
 def get_twitter_followers(twitter_handle: str) -> str:
-    url: str = f"https://twitter.com/{twitter_handle}"
+    url: str = f"https://mobile.twitter.com/{twitter_handle}"
     r = requests.get(url, headers=HEADERS)
     soup = BeautifulSoup(r.text, "html.parser")
-    followers: str = soup.find("a", {"href": f"/{twitter_handle}/followers"}).findAll("span")[-1].attrs["data-count"]
+    followers: str = soup.find("a", {"href": f"/{twitter_handle}/followers"}).text.strip(" Followers \n")
     return followers
 
 
@@ -40,9 +40,9 @@ def get_keybase_followers(keybase_handle: str) -> str:
     return followers
 
 
-def get_linkedin_views(username: str, password: str, profile_handle: str) -> Tuple[str, str, str]:
+def get_linkedin_views(username: str, password: str, profile_handle: str) -> Tuple[str, str, str, str, str]:
     opts = Options()
-    opts.headless = True
+    opts.headless = False
     browser = Firefox(options=opts)
     browser.get("https://linkedin.com/login")
     browser.find_element_by_name("session_key").send_keys(username)
@@ -53,18 +53,23 @@ def get_linkedin_views(username: str, password: str, profile_handle: str) -> Tup
     post_views = browser.find_element_by_class_name("update-views").text.split("\n")[0]
     profile_views = browser.find_element_by_class_name("profile-views").text.split("\n")[0]
     search_appearances = browser.find_element_by_class_name("search-appearances").text.split("\n")[0]
-    return profile_views, post_views, search_appearances
+    followers = browser.find_element_by_class_name("pv-recent-activity-section__follower-count").text.split(" ")[0]
+    sleep(2)
+    browser.get("https://www.linkedin.com/mynetwork/")
+    connections = browser.find_element_by_css_selector("#ember1193 > div:nth-child(1) > div:nth-child(2)").text
+    return profile_views, post_views, search_appearances, followers, connections
 
 
 def get_github_stats(github_username: str) -> Tuple[str, str]:
     r = requests.get(f"https://github.com/{github_username}", headers=HEADERS)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    followers = soup.findAll('span', {'class': 'Counter'})[-2].text.strip('\n').strip()
-    following = soup.findAll('span', {'class': 'Counter'})[-1].text.strip('\n').strip()
+    soup = BeautifulSoup(r.text, "html.parser")
+    followers = soup.find("a", {"href": f"/{github_username}?tab=followers"}).text.strip("followers\n").strip()
+    following = soup.find("a", {"href": f"/{github_username}?tab=following"}).text.strip("following\n").strip()
     return followers, following
 
 
 def run():
+    print("running socialkarma")
     twitter_handle = get_environment_variable("TWITTER_HANDLE")
     keybase_handle = get_environment_variable("KEYBASE_HANDLE")
     linkedin_username = get_environment_variable("LINKEDIN_USERNAME")
@@ -76,12 +81,20 @@ def run():
     result = []
     timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     try:
+        print("getting twitter followers")
         twitter_followers = get_twitter_followers(twitter_handle)
         result.append({"type": "twitter", "timestamp": timestamp, "twitter_followers": twitter_followers})
         keybase_followers = get_keybase_followers(keybase_handle)
         result.append({"type": "keybase", "timestamp": timestamp, "keybase_followers": keybase_followers})
         github_followers, github_following = get_github_stats(github_username)
-        result.append({"type": "github", "timestamp": timestamp, "github_followers": github_followers, "github_following": github_following})
+        result.append(
+            {
+                "type": "github",
+                "timestamp": timestamp,
+                "github_followers": github_followers,
+                "github_following": github_following,
+            }
+        )
         stackoverflow_reputation, stackoverflow_profile_views = get_stackoverflow_followers(stackoverflow_handle)
         result.append(
             {
@@ -91,7 +104,7 @@ def run():
                 "stackoverflow_profile_views": stackoverflow_profile_views,
             }
         )
-        linkedin_profile_views, linkedin_post_views, linkedin_search_appearances = get_linkedin_views(
+        linkedin_profile_views, linkedin_post_views, linkedin_search_appearances, linkedin_followers, linkedin_connections = get_linkedin_views(
             linkedin_username, linkedin_password, linkedin_profile_handle
         )
         result.append(
@@ -101,6 +114,8 @@ def run():
                 "linkedin_profile_views": linkedin_profile_views,
                 "linkedin_post_views": linkedin_post_views,
                 "linkedin_search_appearances": linkedin_search_appearances,
+                "linkedin_followers": linkedin_followers,
+                "linkedin_connections": linkedin_connections,
             }
         )
     except Exception as e:
