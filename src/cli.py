9aa5835e950e9603d65039d0c377d5a683372486
Anubhav Y <contact+code@anubhav.codes>
2020-05-13 10:36:27 +0200

    💂🏽‍♀️ Change array to string.

--- src/cli.py
+++ src/cli.py
@@ -7,9 +7,9 @@ from service import process_source, SOURCES
 @click.option("--id", required=True, help="id of the source")
 @click.option("--username", required=False, default=None, help="username of the source")
 @click.option("--password", required=False, default=None, help="password of the source")
-@click.option("--host", required=True, default="localhost", help="host of mongo instance")
-@click.option("--port", required=True, default=27017, help="port number of the mongo instance")
-@click.option("--db", required=True, default="stats", help="db to use for mongo instance")
+@click.option("--host", required=False, default="localhost", help="host of mongo instance")
+@click.option("--port", required=False, default=27017, help="port number of the mongo instance")
+@click.option("--db", required=False, default="stats", help="db to use for mongo instance")
 @click.option("--quite/--no-quite", default=False)
 @click.argument("source", type=click.Choice(SOURCES.keys()))
 def run(id, username, password, host, port, db, quite, source):
